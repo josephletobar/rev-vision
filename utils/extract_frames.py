9ea1059c4d6507390ad50899cc102b9182e5b5ed
Joseph <162514273+josephletobar@users.noreply.github.com>
2025-12-22 23:29:17 -0500

    add YOLO ball detectiob

--- utils/extract_frames.py
+++ utils/extract_frames.py
@@ -32,4 +32,4 @@ def extract_frames(video_path, output_dir, step=30):
         cap.release()
 
 ROOT = Path().resolve().parent
-extract_frames(f"outputs", "data/perspective_frames", step=2)
+extract_frames(f"test_videos", "data/raw_frames", step=10)
